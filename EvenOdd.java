2b2ef5644ebf430ddbeb112221bad0d93e0b0fad
hisoka7i <rohit7i@outlook.com>
2023-08-12 10:34:46 +0530

    Even odd number using bit operator

---
+++ EvenOdd.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class EvenOdd {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter your number here: ");
+
+        try{
+            int number = scan.nextInt();
+            if((number&1)==0){
+                System.out.println("Number is even");
+            }else{
+                System.out.println("Number is odd");
+            }
+        }catch(Exception e){
+            System.out.println("Please make sure that you entered a interger");
+        }
+    }
+}
