05e9ef53a862fb7549637be249084844e05b4438
hisoka7i <rohit7i@outlook.com>
2023-09-01 14:04:19 +0530

    hi

---
+++ a55.java
@@ -0,0 +1,14 @@
+public class a55{
+  public static void main(String[] args){
+    int nums = {1,1,2,5,3,1,0,0,1,3};
+    int n = nums.length;
+    int reach = 0;
+    for(int i=0;i<n;i++){
+      if(reach<i){
+        return false;
+      }
+      reach = Math.max(reach,i+nums[i]);
+    }
+    return true;
+  }
+}
