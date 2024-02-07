82f0efa8cf77a5c7b86dbd346b53b7aa20813f85
Rohit Srivastav <60313431+hisoka7i@users.noreply.github.com>
2024-02-07 21:15:36 +0530

    for all possible permutations of a string

---
+++ Permutations.java
@@ -0,0 +1,18 @@
+package String;
+
+public class Permutations {
+    public static void main(String[] args) {
+        inOrderPermutation("", "null");
+    }
+    public static void inOrderPermutation
+    (String prefix, String sample){
+        int len = sample.length();
+        if(len==0){
+            System.out.println(">>>"+prefix);
+        }else{
+        for(int i=0;i<len;i++){
+        inOrderPermutation(prefix+sample.charAt(i), sample.substring(0, i)+sample.substring(i+1, len));
+        }
+    }
+    }
+}
