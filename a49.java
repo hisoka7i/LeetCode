22cf111a2bab65fa82915f3afde71905ec0ee70f
Rohit Srivastav <60313431+hisoka7i@users.noreply.github.com>
2024-02-09 21:52:33 +0530

    6 ms

--- a49.java
+++ a49.java
@@ -1,37 +1,26 @@
 import java.util.*;
 
 public class a49 {
-    static List<List<String>> output = new ArrayList<>();
     static List<String> sampleList = null;
     public static void main(String[] args) {
         String cat = "cat";
         String[] sample = {"eat","tea","tan","ate","nat","bat"};
         sampleList = Arrays.asList(sample);
-        Map<String,List<Integer>> filledMap = fillMap(sampleList);
-        
-        List<List<String>> output = new ArrayList<>();
-        for(Map.Entry<String,List<Integer>> map: filledMap.entrySet()){
-            List<Integer> positions = map.getValue();
-            List<String> groupanagrams = new ArrayList<>();
-            for(Integer i: positions){
-                groupanagrams.add(sampleList.get(i));
-            }
-            output.add(groupanagrams);
-        }
-        System.out.println(output);
+        Map<String,List<String>> filledMap = fillMap(sampleList);
+        System.out.println(new ArrayList<>(filledMap.values()));
     }
-    public static Map<String,List<Integer>> fillMap(List<String> sample){
-        Map<String,List<Integer>> map = new HashMap<>();
+    public static Map<String,List<String>> fillMap(List<String> sample){
+        Map<String,List<String>> map = new HashMap<>();
         for(int i=0;i<sample.size();i++){
             char[] ch = sample.get(i).toCharArray();
             Arrays.sort(ch);
             String currentSortedString = String.valueOf(ch);
             if(!map.containsKey(currentSortedString)){
-                List<Integer> pos = new ArrayList<>();
+                List<String> pos = new ArrayList<>();
                 map.put(currentSortedString, pos);
             }
-            List<Integer> pos = map.get(currentSortedString);
-            pos.add(i);
+            List<String> pos = map.get(currentSortedString);
+            pos.add(sample.get(i));
             map.put(currentSortedString, pos);
         }
         return map;
