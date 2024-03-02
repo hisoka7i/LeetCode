64868e21970f4e5c7c76ae74cad6317101ce1261
Rohit Srivastav <60313431+hisoka7i@users.noreply.github.com>
2024-03-02 23:21:57 +0530

    0 ms

---
+++ a88.java
@@ -0,0 +1,29 @@
+public class a88 {
+    public static void main(String[] args) {
+        
+    }
+    public static void merge(int[] nums1, int m, int[] nums2, int n){
+        int k = m + n - 1;
+        int i = m - 1;
+        int j = n - 1;
+        while(i>=0 && j>=0){
+            if(nums1[i]>nums2[j]){
+                nums1[k]=nums1[i];
+                i--;
+                k--;
+            }else{
+                nums1[k]=nums2[j];
+                j--;
+                k--;
+            }
+        }
+        //if(m!=0 || n!=0){
+    while(i>=0){
+        nums1[k--]=nums1[i--];
+    }
+    while(j>=0){
+        nums1[k--]=nums2[j--];
+    }
+    //Arrays.sort(nums1);
+    }
+}
