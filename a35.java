ad9dc02cdcd1374e69375eb22c548fe4afd97423
Rohit Srivastav <60313431+hisoka7i@users.noreply.github.com>
2024-03-06 23:25:39 +0530

    using binary search 0 ms

---
+++ a35.java
@@ -0,0 +1,16 @@
+public class a35 {
+    public int searchInsert(int[] nums, int target) {
+        int ans = nums.length;
+        int left =0, right = nums.length-1;
+        while(left<=right){
+            int mid = (left+right)/2;
+            if(nums[mid]>=target){
+                ans = mid;
+                right = mid - 1;
+            }else{
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
