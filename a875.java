a2e6d96cf3d491d2557e6f6f7c6ab485d14f2e29
Rohit Srivastav <60313431+hisoka7i@users.noreply.github.com>
2024-03-16 00:19:37 +0530

    binary search implemented

--- a875.java
+++ a875.java
@@ -1,3 +1,32 @@
+import java.util.Arrays;
+
 public class a875 {
-    
+    public static void main(String[] args) {
+        int[] sample = {3,6,7,11};
+        System.out.println(minEatingSpeed(sample, 8));
+    }
+    public static int minEatingSpeed(int[] piles, int h) {
+        Arrays.sort(piles);
+        int low = 0, high = piles[piles.length-1];
+        int ans = Integer.MAX_VALUE;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(findValidK(piles, high, mid)<=h) {
+                System.out.println("working");
+                ans = mid;
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+    public static int findValidK(int[] piles, int h, int mid){
+        int countHrs  = 0;
+        for(int i=0;i<piles.length;i++){
+            //we are making it double to get the exact answerr
+            countHrs += Math.ceil((double)piles[i]/(double)mid);
+        }
+        return countHrs;
+    }
 }
