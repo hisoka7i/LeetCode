17f731bbaa2f510c4bae86dd817d5a90ccda73ba
hisoka7i <rohit7i@outlook.com>
2024-04-05 22:19:22 +0530

    This is a interesting question

---
+++ BinarySearch/BS2D/a240.java
@@ -0,0 +1,21 @@
+package BinarySearch.BS2D;
+
+public class a240 {
+    public  boolean searchMatrix(int[][] matrix, int target) {
+        //I have attached image as well, which explain its solution
+        //we have to assume starting position such that we have ascending or decending data
+        //when we move across rows then to column
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int row = 0, col = m-1;
+        while(row<n && col>=0){
+            if(matrix[row][col]==target)return true;
+            if(matrix[row][col]>target){
+                col--;
+            }else{
+                row++;
+            }
+        }
+        return false;
+    }
+}
