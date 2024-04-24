5c47c56cbe470bed2b7daf159fcb3d245dacd984
hisoka7i <rohit7i@outlook.com>
2024-04-25 00:27:11 +0530

    solution added with priority queue as well

--- String/Medium/a451.java
+++ String/Medium/a451.java
@@ -1,12 +1,38 @@
 package String.Medium;
 
+import java.util.HashMap;
+import java.util.PriorityQueue;
+
+
 public class a451 {
     public static void main(String[] args) {
         
     }
+    //leetcode solution using priority queue
+    public String frequencySort(String s) {
+        StringBuilder sb = new StringBuilder();
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0)+1);
+        }
+
+        PriorityQueue<Character> queue = new PriorityQueue<>((x,y)->map.get(y)-map.get(x));
+        for(char c: map.keySet()){
+            queue.offer(c);
+        }
+        while(!queue.isEmpty()){
+            char ch = queue.poll();
+            for(int i=0;i<map.get(ch);i++){
+                sb.append(ch);
+            }
+        }
+        return sb.toString();
+    }
+
+
     //mysolution was to make a trie and then add, values into it
     //Then make a Node and add character nodes into it according to count
-    public static String frequencySort(String s){
+    public static String frequencySort1(String s){
         String outString="";
         int[] trie = new int[26];
         for(int i=0;i<s.length();i++){
