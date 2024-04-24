47d7298b837dcdf4816897b008de9d288c3592bf
hisoka7i <rohit7i@outlook.com>
2024-04-24 23:18:19 +0530

    solution using trie and node

---
+++ String/Medium/a451.java
@@ -0,0 +1,64 @@
+package String.Medium;
+
+public class a451 {
+    public static void main(String[] args) {
+        
+    }
+    //mysolution was to make a trie and then add, values into it
+    //Then make a Node and add character nodes into it according to count
+    public static String frequencySort(String s){
+        String outString="";
+        int[] trie = new int[26];
+        for(int i=0;i<s.length();i++){
+            trie[s.charAt(i)-'a']++;
+        }
+        for(int i=0;i<26;i++){
+            if(trie[i]!=0){
+                
+            }
+        }
+        return outString;
+    }
+    public void insertNode(StringNode node,char ch,int count){
+        StringNode temp = node;
+        if(temp==null){
+            StringNode newNode = new StringNode(ch,count);
+            node = newNode;
+        }
+        while(temp!=null){
+            if(temp.next==null){
+                if(temp.count>count){
+                    temp.next = temp;
+                }else{
+                    StringNode newHead = new StringNode(ch, count);
+                    newHead.next = temp;
+                    node = newHead;
+                }
+            }
+            else{
+                if(temp.next.count<count){
+                    StringNode newNode = new StringNode(ch, count);
+                    if(temp.next.next==null){
+                        temp.next =newNode;
+                    }else{
+                        newNode.next = temp.next.next;
+                        temp.next = newNode;
+                    }
+                }else if(temp.count<count){
+                    StringNode newNode = new StringNode(ch, count);
+                    newNode.next = temp;
+                    node = newNode;
+                }
+            }
+        }
+    }
+}
+class StringNode{
+    char ch;
+    int count;
+    StringNode next;
+    StringNode(char ch,int count){
+        this.ch=ch;
+        this.count=count;
+    }
+}
