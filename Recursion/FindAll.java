9589d6e93bd4176f84e1f6c2e467ea1c5c4bab8f
hisoka7i <rohit7i@outlook.com>
2024-05-23 23:28:51 +0530

    another function added to find all using recursion

--- Recursion/FindAll.java
+++ Recursion/FindAll.java
@@ -1,9 +1,11 @@
 import java.util.List;
 import java.util.ArrayList;
 public class FindAll {
+    private static List<Integer> allIndexes= new ArrayList<>();
     public static void main(String[] args){
         int[] arr = {1,2,4,2,3,4,5,4};
-        System.out.println(findAll(arr,2,0));
+        // System.out.println(findAll(arr,2,0));
+        System.out.println(findAllGlobal(arr, 2, 0));
     }
     static List<Integer> findAll(int[] nums,int target, int index){
         List<Integer> list = new ArrayList<>();
@@ -16,6 +18,14 @@ public class FindAll {
         List<Integer> answersFromBelow = findAll(nums, target, index+1);
         list.addAll(answersFromBelow);
         return list;
-
     }
+
+    static List<Integer> findAllGlobal(int[] nums, int target, int index){
+        if(index>nums.length-1){
+            return allIndexes;
+        }
+        if(nums[index]==target)allIndexes.add(index);
+        return findAllGlobal(nums, target, index+1);
+    }
+
 }
