28418219f0d75704cc785cc921c4e4e7419bed44
hisoka7i <rohit7i@outlook.com>
2024-05-24 22:02:42 +0530

    implement queue using stack

--- StackAndQueue/a225.java
+++ StackAndQueue/a225.java
@@ -11,6 +11,9 @@ class MyStack {
     
     public void push(int x) {
         q.add(x);
+        //here we are starting with 1 because we do not want to, reverse till the 
+        //second last element
+        //other wise if we reverse the entire queue, then it will come to its intial stage
         for(int i=0;i<q.size();i++){
             //here we are reversing the queue
             q.add(q.remove());
