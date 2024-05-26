5b2c640c5e135c456828c9ace3aaa8b366e225e9
hisoka7i <rohit7i@outlook.com>
2024-05-26 21:56:26 +0530

    dry run it, very important

--- StackAndQueue/a155.java
+++ StackAndQueue/a155.java
@@ -1,5 +1,40 @@
 package StackAndQueue;
 
-public class a155 {
+import java.util.Stack;
+
+class MinStack {
+    public static void main(String[] args) {
+        int[] integer_array = {-2,0,4,-3,9,-10};
+        for(int i: integer_array){
+            push(i);
+        }
+        System.out.println(st.toString());
+        pop();
+        System.out.println(min);
+        System.out.println(st.toString());
+    }
+    static Stack<Integer> st = new Stack<>();
+    static int min = Integer.MAX_VALUE;
     
+    public static void push(int val) {
+        if(val <= min){
+            //pushing the old minimum value
+            st.push(min);
+            min = val;
+        }
+        st.push(val);
+    }
+    
+    public static void pop() {
+        //reverting to previous minimum
+        if(st.pop() == min)min = st.pop();
+    }
+    
+    public static int top() {
+        return st.peek();
+    }
+    
+    public static int getMin() {
+        return min;
+    }
 }
