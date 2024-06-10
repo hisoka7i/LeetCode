a023b09af47206726b6e748587c26a94330f99a9
hisoka7i <rohit7i@outlook.com>
2024-06-10 21:00:16 +0530

    Striever solution for maximal rectangle

--- StackAndQueue/a85.java
+++ StackAndQueue/a85.java
@@ -3,60 +3,70 @@ package StackAndQueue;
 import java.util.Stack;
 
 public class a85 {
-    // Largest Rectangle in Histogram
     public static void main(String[] args) {
-        
+        int[][] mar = new int[3][4];
+        System.out.println(mar.length);
     }
+    //Maximal Rectangle
+
+    public static int maximalRectangle(char[][] matrix) {
+        int maxArea = 0;
+        //for every row we need to find the largest histogram
+        //we already have a function to return largest area of histogram
+        //try to draw above lines on a sheet of paper, to help visualise
+        int[] histogram = new int[matrix[0].length];
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(matrix[i][j]=='1')
+                    histogram[j]++;
+                else
+                    histogram[j]=0;
+            }
+            int area = largestRectangleArea(histogram);
+            maxArea = Math.max(maxArea, area);
+        }
+        return maxArea;
+    }
+
     public static int largestRectangleArea(int[] heights) {
-     //we will make a previous smaller stack 
-     int n = heights.length;
-     int[] left_smaller = new int[n];
+        int n = heights.length;
 
-     //we will make a next smaller stack
-     int[] right_smaller = new int[n];
+        //array to maintain left minimum
+        int[] left_minimum = new int[n];
 
-     //for left smaller we will iterate from left compare with stack for the prev smaller 
-     //for right smaller we will iterate from right and compare with stack for the next higher
-     
-     //we will create a stack which will keep track of the indexes
-     Stack<Integer> indexes = new Stack<>();
+        //array to maintain right minimum 
+        int[] right_minimum = new int[n];
+        //stack to maintain, indexes
+        Stack<Integer> stack = new Stack<>();
 
-     //formula that we will implement is right_smaller - left_smaller + 1 * width
-
-
-     //making a loop for the left smaller
-     for(int i=0;i<n;i++){
-        while(!indexes.isEmpty() && (heights[indexes.peek()]>=heights[i])){
-            indexes.pop();
+        for(int i=0;i<n;i++){
+            while(!stack.isEmpty() && heights[stack.peek()]>=heights[i]){
+                stack.pop();
+            }
+            if(stack.isEmpty())
+                left_minimum[i]=0;
+            else
+                left_minimum[i]=stack.peek()+1;
+            stack.push(i);
         }
-        if(indexes.isEmpty())
-            left_smaller[i]=0;
-        else 
-            left_smaller[i]=indexes.peek()+1;
-        indexes.push(i);
-     }
-     //clearing the stack
-     indexes.clear();
-     //making a loop for right smaller
-     for(int i=(n-1);i>=0;i--){
-        while(!indexes.isEmpty() && (heights[indexes.peek()]>=heights[i])){
-            indexes.pop();
+        stack.clear();
+        for(int j=n-1;j>=0;j--){
+            while(!stack.isEmpty() && heights[stack.peek()]>=heights[j]){
+                stack.pop();
+            }
+            if(stack.isEmpty())
+                right_minimum[j] = n-1;
+            else   
+                right_minimum[j] = stack.peek()-1;
+            stack.push(j);
         }
-        if(indexes.isEmpty())
-            right_smaller[i] = n-1;
-        else   
-            right_smaller[i] = indexes.peek()-1;
 
-        indexes.push(i);
-     }
+        //iterating through the array to find the max area 
+        int maximum = 0;
+        for(int i=0;i<n;i++){
+            maximum = Math.max(maximum, (right_minimum[i]-left_minimum[i]+1)*heights[i]);
+        }
 
-
-     //since now we have left smaller and right smaller
-     int max_histogram = 0;
-     for(int i=0;i<n;i++){
-        max_histogram = Math.max(max_histogram, (heights[i]*(right_smaller[i]-left_smaller[i]+1)));
-     }
-
-     return max_histogram;
+        return maximum;
     }
 }
