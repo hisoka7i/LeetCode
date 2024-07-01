f643c93af215664b234f44c0a84d3d8b2444fbe6
hisoka7i <rohit7i@outlook.com>
2024-07-01 23:12:25 +0530

    This is solution to, leetcode daily challenge

---
+++ LeetcodeStreak/Easy/a1550.java
@@ -0,0 +1,23 @@
+
+public class a1550 {
+    //Given an integer array arr, return true if there 
+    //are three consecutive odd numbers in the array. Otherwise, return false.
+    public static void main(String[] args) {
+        
+    }
+    public static boolean threeConsecutiveOdds(int[] arr) {
+        if(arr.length < 3){
+            return false;
+        }
+        int l = 0, r = 2;
+        while(r<arr.length){
+            if(arr[l]%2==1 && arr[r]%2==1){
+                if(arr[l+1]%2==1)
+                    return true;
+            }
+            l++;
+            r++;
+        }
+        return false;
+    }
+}
