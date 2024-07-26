f0ea497e0437768de0c5414b04f6548df586310b
hisoka7i <rohit7i@outlook.com>
2024-07-26 21:42:07 +0530

    tree traveral with recursion

---
+++ DSAConcepts/TreeTraversal/BinaryTree.java
@@ -0,0 +1,12 @@
+package TreeTraversal;
+
+public class BinaryTree {
+    public int data;
+    public BinaryTree left;
+    public BinaryTree right;
+    public BinaryTree(int data, BinaryTree left, BinaryTree right){
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+}
---
+++ DSAConcepts/TreeTraversal/WithRecursion/Traversal.java
@@ -0,0 +1,29 @@
+package TreeTraversal.WithRecursion;
+
+import TreeTraversal.BinaryTree;
+
+public class Traversal {
+    public void PreOrder(BinaryTree root){
+        if(root == null)
+            return;
+        System.out.println(root.data);
+        PreOrder(root.left);
+        PreOrder(root.right);
+    }
+
+    public void InOrder(BinaryTree root){
+        if(root==null)
+            return;
+        InOrder(root.left);
+        System.out.println(root.data);
+        InOrder(root.right);
+    }
+
+    public void PostOrder(BinaryTree root){
+        if(root == null)
+            return;
+        PostOrder(root.left);
+        PostOrder(root.right);
+        System.out.println(root.data);
+    }
+}
