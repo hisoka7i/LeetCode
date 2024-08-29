9e879a52f840f9207d2d2fb08c3433bb37e07027
hisoka7i <rohit7i@outlook.com>
2024-08-29 21:38:26 +0530

    Striver heap hard question

--- HeapAndPQ/PQ/Easy/a703.java
+++ HeapAndPQ/PQ/Easy/a703.java
@@ -12,7 +12,6 @@ class KthLargest {
         }
     }
     
-    //This is showing time limit exceeded issue.
     public int add(int val) {
         if(min_heap.size() < size){
             min_heap.offer(val);
@@ -22,4 +21,29 @@ class KthLargest {
         }
         return min_heap.peek();
     }
+    //My solution, but this is showing out of memory issue. 
+    // I should have start from the very beginning.
+    /*
+     * public KthLargest(int k, int[] nums) {
+        size = k;
+        for(int current: nums){
+            max_heap.offer(current);
+        }
+    }
+    
+    public int add(int val) {
+        max_heap.offer(val);
+        if(max_heap.size() >= size){
+            int[] deleted_value = new int[size];
+            for(int i=0;i<size;i++){
+                deleted_value[i] = max_heap.poll();
+            }
+            for(int current_value: deleted_value){
+                max_heap.offer(current_value);
+            }
+            return deleted_value[deleted_value.length - 1];
+        }
+        return 0;
+    }
+    */
 }
