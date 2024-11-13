cdf802d0908526114a22ae275ba1496bc836fd55
hisoka7i <rohit7i@outlook.com>
2024-11-14 00:06:28 +0530

    binary tree easy solution

---
+++ BinaryTree/Easy/a100.java
@@ -0,0 +1,11 @@
+package BinaryTree.Easy;
+
+import BinaryTree.TreeNode;
+
+public class a100 {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        
+    }
+
+    public boolean isReallySame()
+}
