4b7d4c8e540556d1f345ac9c6d583ed373126407
hisoka7i <rohit7i@outlook.com>
2024-11-14 00:27:22 +0530

    bt zig zag traversal

--- BinaryTree/Easy/a100.java
+++ BinaryTree/Easy/a100.java
@@ -1,11 +1,31 @@
 package BinaryTree.Easy;
 
+import com.sun.tools.javac.resources.legacy;
+
 import BinaryTree.TreeNode;
+import java.awt.desktop.QuitHandler;
 
 public class a100 {
+
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        
+        boolean finalvalue = isReallySame(p, q);
+        return finalvalue;
     }
 
-    public boolean isReallySame()
+    public boolean isReallySame(TreeNode p, TreeNode q) {
+        if (p == null && q != null) {
+            return false;
+        }
+        if (p != null && q == null) {
+            return false;
+        }
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+
+        return (isReallySame(p.left, q.left) && isReallySame(p.right, q.right));
+    }
 }
---
+++ BinaryTree/Medium/a103.java
@@ -0,0 +1,51 @@
+package BinaryTree.Medium;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+import BinaryTree.TreeNode;
+
+public class a103 {
+    //in this we will use, lvl order traversal, and when travel even level we will use a stack to reverse the values
+    //or we can also do collection.reverse
+    
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        //a queue is required to save the values
+        List<List<Integer>> output = new ArrayList<>();
+        if(root == null)
+            return  output;
+        Queue<TreeNode> queue = new PriorityQueue<>();
+        boolean leftToRight = false;
+
+        queue.offer(root);
+        queue.offer(null);
+
+        List<Integer> level_values = new ArrayList<>();
+        while(!queue.isEmpty()){
+            TreeNode current = queue.poll();
+            if(current != null){
+                    if(current.right != null){
+                        queue.offer(current.right);
+                    }
+                    if(current.left != null){
+                        queue.offer(current.left);
+                    }
+                level_values.add(current.val);
+            }else{
+                if (!leftToRight) {
+                Collections.reverse(level_values);
+            }
+                output.add(level_values);
+                if(!queue.isEmpty()){
+                    queue.offer(null);
+                    level_values = new ArrayList<>();
+                    leftToRight = !leftToRight;
+                }
+            }
+        }
+        return  output;
+    }
+}
