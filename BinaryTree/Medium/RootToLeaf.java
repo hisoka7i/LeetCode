8313f50dadcf3acd769092a91316eaa05c277808
hisoka7i <rohit7i@outlook.com>
2024-11-21 23:08:21 +0530

    All the path to leaf, using backtracking

---
+++ BinaryTree/Medium/RootToLeaf.java
@@ -0,0 +1,40 @@
+package BinaryTree.Medium;
+
+import java.util.ArrayList;
+
+// import org.w3c.dom.Node;
+import BinaryTree.TreeNode;
+import java.util.List;
+import javax.naming.spi.DirStateFactory;
+
+public class RootToLeaf {
+
+    private static List<List<Integer>> result = new ArrayList<>();
+
+    //all root to leave paths
+    public static List<List<Integer>> Paths(TreeNodede root) {
+        // code here
+        allThePaths(new ArrayList<>(), root);
+        return result;
+    }
+
+    public static void allThePaths(List<Integer> path, TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        //we will add current value in the path
+        path.add(root.val);
+        if (root.left == null && root.right == null) {
+            //this means that this a leaf node.
+            //we need to add it's path into the result
+            result.add(new ArrayList<>(path));
+        } else {
+//now traverse to left
+            allThePaths(path, root.left);
+            allThePaths(path, root.right);
+        }
+        //do the backtracking 
+        path.remove(path.size() - 1);
+    }
+}
