6657ba886c4b5d3574791d0cefe630e049b4c7df
hisoka7i <rohit7i@outlook.com>
2024-12-08 15:34:47 +0530

    find kth smallest in bst

--- BinarySearchTree/Medium/a230.java
+++ BinarySearchTree/Medium/a230.java
@@ -3,23 +3,29 @@ package BinarySearchTree.Medium;
 import BinaryTree.TreeNode;
 
 public class a230 {
+    int count;
     int answer = -1;
     public int kthSmallest(TreeNode root, int k) {
-        inorder(root, k);
+        count = k;
+        inorder(root);
         return answer;
     }
 
     //A simple in-order traversal will give me sorted answer, I simple need to get (k-1)
-    public void inorder(TreeNode root, int k){
+    public void inorder(TreeNode root){
         if(root == null)
             return;
-        if(k == 0){
+
+        //move towards left
+        inorder(root.left);
+
+        //decreement the count
+        count--;
+        if(count == 0){
             answer = root.val;
             return;
-        }  
-        else{
-            inorder(root.left, k-1);
-            inorder(root.right, k-1);
         }
+
+        inorder(root.right);
     }
 }
