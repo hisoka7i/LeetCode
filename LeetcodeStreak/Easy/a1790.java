e9c707160f5f0bd4b4a56e70c6cbb1ca7fee0e04
hisoka7i <rohit7i@outlook.com>
2025-02-05 13:52:07 +0530

    One String swap

---
+++ LeetcodeStreak/Easy/a1790.java
@@ -0,0 +1,38 @@
+package LeetcodeStreak.Easy;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class a1790 {
+    public boolean areAlmostEqual(String s1, String s2){
+        if(s1.length() != s2.length())return false;
+
+        int count = 0;
+        int firstIndex 
+    }
+
+    // This is only failing 2/138 test cases
+    public boolean areAlmostEqual2(String s1, String s2) {
+        if (s1.length() != s2.length())
+            return false;
+        int count = 0;
+        int firstIndex = 0;
+        int secondIndex = 0;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                count++;
+                if (count > 2)
+                    return false;
+                if (count == 1) {
+                    firstIndex = i;
+                } else {
+                    secondIndex = i;
+                }
+            }
+        }
+
+        return ((s1.charAt(firstIndex) == s2.charAt(secondIndex) && (s2.charAt(secondIndex) == s1.charAt(firstIndex))));
+    }
+}
