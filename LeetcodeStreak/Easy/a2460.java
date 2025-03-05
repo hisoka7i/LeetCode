70a3c843ee178f8501f2a6db58e690ae151c7877
Rohit Srivastava <60313431+hisoka7i@users.noreply.github.com>
2025-03-05 17:45:56 +0530

    Update a2460.java

--- LeetcodeStreak/Easy/a2460.java
+++ LeetcodeStreak/Easy/a2460.java
@@ -3,18 +3,20 @@ package LeetcodeStreak.Easy;
 public class a2460 {
     public int[] applyOperations(int[] nums) {
         int length = nums.length;
-        int count =0;
         for(int i=0;i<length-1;i++){
             if(nums[i] == nums[i+1]){
                 nums[i] = nums[i]*2;
                 nums[i+1] = 0;
-                count++;
                 continue;
             }
-            if(nums[i] == 0){
-                count++;
+        }
+        int[] output = new int[length];
+        int i2 = 0;
+        for(int i=0;i<length;i++){
+            if(nums[i] != 0){
+                output[i2++] = nums[i];
             }
         }
-        
+        return output;
     }
 }
