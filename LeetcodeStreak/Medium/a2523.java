b08259a8119b1d5ca75963ea1786d08ff3d97772
hisoka7i <rohit7i@outlook.com>
2025-04-07 22:54:46 +0530

    medium solved

--- LeetcodeStreak/Medium/a2523.java
+++ LeetcodeStreak/Medium/a2523.java
@@ -1,5 +1,7 @@
 package LeetcodeStreak.Medium;
 
+import java.util.ArrayList;
+
 public class a2523 {
     public static void main(String[] args) {
         System.out.println(isPrime(11));  // true
@@ -7,8 +9,38 @@ public class a2523 {
         System.out.println(isPrime(29));   // true
         System.out.println(isPrime(97));  // true
     }
-    // int lowest = 0;
+    //one solution is to find all the possible prime numbers and then simple find the pair with the minimum difference
     public int[] closestPrimes(int left, int right) {
+        ArrayList<Integer> primes = new ArrayList<>();
+        
+        for (int i = left; i <= right; i++) {
+            if (isPrime(i)) {
+                primes.add(i);
+            }
+        }
+    
+        if (primes.size() < 2) {
+            return new int[]{-1, -1};
+        }
+    
+        int[] output = new int[2];
+        int minDiff = Integer.MAX_VALUE;
+    
+        for (int i = 0; i < primes.size() - 1; i++) {
+            int j = i + 1;
+            // now I simply need to find pair which has the smallest difference
+            int diff = primes.get(j) - primes.get(i);
+            if (diff < minDiff) {
+                minDiff = diff;
+                output[0] = primes.get(i);
+                output[1] = primes.get(j);
+            }
+        }
+    
+        return output;
+    }
+    // int lowest = 0;
+    public int[] closestPrimes2(int left, int right) {
         int n1 = findFloor(left, right); 
         if(n1 == -1){
             return new int[]{-1,-1};
