5ae410145a873a56e205fbe1aee95f9f9afef11b
hisoka7i <rohit7i@outlook.com>
2025-04-20 23:00:37 +0530

    flipkart questions

--- Revision/Matrix/RottingOranges.java
+++ Revision/Matrix/RottingOranges.java
@@ -1,38 +1,65 @@
 package Revision.Matrix;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.Stack;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class RottingOranges {
     public int orangesRotting(int[][] grid) {
+        return  bruteForceSolution(grid);
     }
 
     //my brute force solution
     public int bruteForceSolution(int[][] grid){
+        // I was going for the right solution but I did not spend enough time on it. 
+        //should have spent enough time on it
         int time = 0;
-        Stack<List<int[]>> totrace = new Stack<>();
-        //now we need to add rotten tomatoes in the list and then add it into the stack
-        List<int[]> rottenPositions = new ArrayList<>();
-        //we will require a set, that is going to keep trace of the visited positions 
-        Set<int[]> set = new HashSet<>();
-        for(int i=0;i<grid.length; i++){
-            for(int j=0;j<grid[0].length; j++){
+        Queue<int[]> rottenPositions = new LinkedList<>();
+        int countFresh = 0;
+        int countRotten = 0;
+        //sabse pehle saare rotten daal do apni queue mai
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
                 if(grid[i][j] == 2){
-                    //which means that the tomato is rotten
-                    rottenPositions.add(new int[]{i,j}); 
-                    set.add(new int[]{i,j});
+                    rottenPositions.add(new int[]{i,j});
+                }else if(grid[i][j] == 1){
+                    countFresh++;
                 }
             }
         }
-        totrace.add(rottenPositions);
 
-        //we will req
+        if(countFresh == 0)return  0;
 
-        //now if the stack is not empty, 
+        //now we need to make a array for directions so that from a point we can move in all the four directions
+        int[] dx = {-1,1,0,0}; //this is to move left and then right
+        int[] dy = {0,0,-1,1}; //this is to move from bottom and then top
+
+        while (rottenPositions.isEmpty() == false) {   
+            int size = rottenPositions.size();
+            countRotten += size; 
+            
+            for(int i=0; i<size; i++){
+                //now we are gonna pull out every rotten oranges 
+                int[] position = rottenPositions.poll();
+                for(int j=0;j<4;j++){
+                    //we are iterate in all for directions to search for fresh oranges
+                    int x = position[0] + dx[j];
+                    int y = position[1] + dy[j];
+                    if(x < 0 || y < 0 || x >= grid[0].length || y >= grid.length || grid[x][y] == 0 || grid[x][y] == 2){
+                        continue;
+                    }
+
+                    //otherwise this means it is a fresh orange which is getting rot
+                    grid[x][y] = 2;
+                    rottenPositions.add(new int[]{x,y});
+                }
+                
+                
+            }
+            if(!rottenPositions.isEmpty())
+                time++; //means go to the next
+        }
+        if(countRotten != countFresh) return  -1;
+
         return time;
     }
 }
