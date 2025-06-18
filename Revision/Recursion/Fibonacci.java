d142fc579ba3d3c0cde3aa5523bc6f09ef375a7a
hisoka7i <rohit7i@outlook.com>
2025-06-18 23:32:34 +0530

    fibonacci series

---
+++ Revision/Recursion/Fibonacci.java
@@ -0,0 +1,10 @@
+package Revision.Recursion;
+
+public class Fibonacci {
+    public int fibo(int n){
+        if (n<=1){
+            return n;
+        }
+        return fibo(n-1) + fibo(n-2);
+    }
+}
