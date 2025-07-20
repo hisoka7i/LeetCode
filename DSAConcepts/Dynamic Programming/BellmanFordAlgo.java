ad2b7e1fb5a8bce3551d7546b18ba6c6700d4ab1
hisoka7i <rohit7i@outlook.com>
2025-07-21 00:06:45 +0530

    bellman ford algo completed

--- DSAConcepts/Dynamic Programming/BellmanFordAlgo.java
+++ DSAConcepts/Dynamic Programming/BellmanFordAlgo.java
@@ -1,5 +1,29 @@
 package Dynamic Programming;
 
+import java.util.*;
 public class BellmanFordAlgo {
-    
+    public int[] shortestPath(int n, int[][] edges, int src){
+        int[] dist = new int[n];
+        Arrays.fill(dist, (int)1e8);
+
+        dist[src] = 0; //we are setting source as zero 
+
+        for(int i = 0; i < n; i ++){
+            //we will be doing relaxation n - 1 times
+            for(int[] edge: edges){
+                int start = edge[0];
+                int end = edge[1];
+                int weight = edge[2];
+
+                if(dist[start] != (int)1e8 && dist[start] + weight < dist[end]){
+                    if( i == n-1){
+                        return  new int[]{-1};
+                    }
+
+                    dist[end] = dist[start]  + weight;
+                }
+            }
+        }
+        return dist;
+    }
 }
